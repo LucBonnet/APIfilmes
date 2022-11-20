fc31b1da9d9ff9fb287cb73e9a60dc57e82bd96b
LucBonnet <lucbonnet10@gmail.com>
2022-11-20 16:29:52 -0300

    allow cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const routes = require("../routes");
 
 const server = express();
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 server.use(express.json());
 
 server.use("/api", routes);
